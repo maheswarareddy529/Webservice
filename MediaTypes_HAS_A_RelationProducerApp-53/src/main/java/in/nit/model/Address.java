84a3b470bac2742da59a3e326acc9971a2cabf13
karra <karra@192.168.0.5>
2020-12-25 13:10:47 +0530

    MediaTypes HAS-A

--- MediaTypes_HAS_A_RelationProducerApp-53/src/main/java/in/nit/model/Address.java
+++ MediaTypes_HAS_A_RelationProducerApp-53/src/main/java/in/nit/model/Address.java
@@ -1,9 +1,7 @@
 package in.nit.model;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import lombok.Data;
 @Data
-@XmlRootElement
 public class Address {
 private String hNo;
 private String loc;
