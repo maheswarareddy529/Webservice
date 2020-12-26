f32ebadc7675168f321281f2d5c413fb19b8c515
karra <karra@192.168.0.5>
2020-12-26 15:52:58 +0530

    Core Java

---
+++ CoreJava-60/src/main/java/in/test/AssignmetOperator.java
@@ -0,0 +1,13 @@
+package in.test;
+
+public class AssignmetOperator {
+
+	public static void main(String[] args) {
+	
+		int a=10;
+		a+=5;//here both values adding.
+		System.out.println(a);//15
+
+	}
+
+}
