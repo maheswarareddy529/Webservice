8ef3918d53895bb530c658fd47bafb5521499038
karra <karra@192.168.0.6>
2021-01-09 12:04:57 +0530

    No web.xml File

---
+++ Jersey_No_Web_xml_AdvancedFirstAopp-71/src/main/java/in/nit/config/AppConfig.java
@@ -0,0 +1,14 @@
+package in.nit.config;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+@ApplicationPath("/rest")//URL Pattern
+public class AppConfig extends ResourceConfig {//FC: ServletContainer
+	//Constructor
+public AppConfig() {
+	//packages("in.nit");//init-param
+	//or
+	this.packages("in.nit");//init-param
+}
+}
