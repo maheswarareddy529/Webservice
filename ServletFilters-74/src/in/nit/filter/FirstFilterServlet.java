7f281c0d99d0535c8abe44272f6e2ebbed735330
karra <karra@192.168.0.3>
2021-01-13 11:42:13 +0530

    Single Filter In Servlet

---
+++ ServletFilters-74/src/in/nit/filter/FirstFilterServlet.java
@@ -0,0 +1,31 @@
+package in.nit.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+@WebFilter("/filter")
+public class FirstFilterServlet implements Filter {
+	public void destroy() {
+	
+	}
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		String name=request.getParameter("user");
+		PrintWriter pw=response.getWriter();
+		if(name.length()==6) {
+		chain.doFilter(request, response);
+		}
+		else {
+			pw.print("Must Enter exact 6 digits username");
+		}
+	}
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
---
+++ ServletFilters-74/src/in/nit/servlet/WelcomeServlet.java
@@ -0,0 +1,27 @@
+package in.nit.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/filter")
+public class WelcomeServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw=response.getWriter();
+		response.setContentType("text/html");
+		String user=request.getParameter("user");
+		String pwd=request.getParameter("pwd");
+		pw.print("Welcome to Servlet Filters!----->>>> "+user+" "+pwd);
+	}
+
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
