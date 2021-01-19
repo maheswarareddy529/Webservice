3f4dc0e044ef1aaa87a298827fafd99deb219f1b
karra <karra@192.168.0.3>
2021-01-19 13:02:01 +0530

    Get One Record from Database

---
+++ StudentFetchingDataProductIntegrationOf_Mysql_Oracle_GetOne_Id-88/src/main/java/in/nit/controller/StudentRestController.java
@@ -0,0 +1,129 @@
+package in.nit.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import in.nit.model.Student;
+import in.nit.service.IStudentService;
+
+@Path("/students")
+public class StudentRestController {
+@Inject//gets impl class Object at runtime	
+private IStudentService service;//HAS-A
+
+//Save Student
+@POST
+@Consumes("application/json")
+public Response saveStudent(Student student) {
+	Response resp=null;
+	try {
+		//Call Service Layer(SL)  to Save
+		Integer count=service.saveStudent(student);
+		if(count>0) {
+			resp=Response.status(Status.CREATED)//201-Success
+					.entity("Student Saved!")
+					.build();
+		}
+		else {
+			resp=Response.status(Status.BAD_REQUEST)//400-Wrong Input
+					.entity("Student Not Saved!")
+					.build();
+		}
+	}
+	catch(Exception e) {
+		resp=Response.status(Status.INTERNAL_SERVER_ERROR)//500-Exception
+				.entity("Student Not Saved!...Error")
+				.build();
+		e.printStackTrace();
+	}
+	return resp;
+}
+//Fetch Student
+@GET
+@Produces("application/json")
+public Response getAllStudents(Student student) {
+	Response resp=null;
+	try {
+		List<Student> list=service.getAllStudents();
+			resp=Response
+					.status(Status.OK)
+					.entity(list)
+					.build();
+	}
+	catch(Exception e) {
+		resp=Response.status(Status.INTERNAL_SERVER_ERROR)//500-Exception
+				.entity("Unable to Fetch Data....")
+				.build();
+		e.printStackTrace();
+	}
+	return resp;
+}
+@DELETE
+@Path("/{id}")//Dynamic path
+public Response deleteOneStudent(
+		@PathParam("id")Integer id
+		) {
+	Response resp=null;
+	try {
+		boolean status=service.removeOneStudent(id);
+		if(status) {
+			resp=Response 
+					   .status(Status.OK)
+					   .entity("Student '"+id+"' Deleted")
+					   .build();
+		}
+		else {
+			resp=Response
+					.status(Status.BAD_REQUEST)
+					.entity("Student '"+id+"' Not Exist")
+					.build();
+		}
+	} catch (Exception e) {
+		resp=Response
+				.status(Status.INTERNAL_SERVER_ERROR)//500-Exception
+				.entity(" Unable to Delete Data!!")
+				.build();
+	   e.printStackTrace();
+	}
+	return resp;
+}
+@GET
+@Path("/{id}")
+@Produces("application/json")
+public Response getOneStudent(
+		@PathParam("id")Integer id
+		) {
+	Response resp=null;
+	try {
+		Student s=service.getOneStudent(id);
+		if(s!=null) {
+			resp=Response
+					.status(Status.OK)
+					.entity(s)
+					.build();
+		}
+		else {
+			resp=Response
+					.status(Status.NO_CONTENT)
+					.build();
+		}
+	} catch (Exception e) {
+		resp=Response
+				.status(Status.INTERNAL_SERVER_ERROR)//500-Exception
+				.entity("Unable To fetch Student Data!!")
+				.build();
+		e.printStackTrace();
+	}
+	return resp;
+}
+}
---
+++ StudentFetchingDataProductIntegrationOf_Mysql_Oracle_GetOne_Id-88/src/main/java/in/nit/service/impl/StudentServiceImpl.java
@@ -0,0 +1,48 @@
+package in.nit.service.impl;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import in.nit.dao.IStudentDao;
+import in.nit.model.Student;
+import in.nit.service.IStudentService;
+
+public class StudentServiceImpl implements IStudentService {
+    @Inject
+	private IStudentDao dao;//HAS-A
+
+	@Override
+	public Integer saveStudent(Student s) {
+		//Calculations
+		Double fee=s.getStdFee();
+		Double dis=fee*10/100.0;
+		s.setStdDiscount(dis);
+		//Calling Data Access layer(DAL)
+		
+		return dao.saveStudent(s);
+	}
+
+	@Override
+	public List<Student> getAllStudents() {
+		List<Student> list=dao.getAllStudents();
+		//Collections.sort(list, (o1,o2)->o1.getStdId()-o2.getStdId());//ID-ASC
+		//Collections.sort(list, (o1,o2)->o2.getStdId()-o1.getStdId());//ID-DESC
+		//Collections.sort(list, (o1,o2)->o1.getStdName().compareTo(o2.getStdName()));//Name-ASC
+		Collections.sort(list, (o1,o2)->o2.getStdName().compareTo(o1.getStdName()));//Name-DESC
+		return list;
+	}
+
+	@Override
+	public boolean removeOneStudent(Integer id) {
+		
+		return dao.removeOneStudent(id);
+	}
+
+	@Override
+	public Student getOneStudent(Integer id) {
+		return dao.getOneStudent(id);
+	}
+	
+}
